a387547c398813b87e166e0f5375878f58d5c893
Sol-Ahn <sorry0194@gmail.com>
2021-05-05 16:54:24 +0900

    Add: section 05

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,84 @@
-'use strict';
+"use strict";
+
+// DOM Manipulation
+
+// Selecting and Manipulating Elements
+/* console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "🎉 Correct Number!";
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+console.log(document.querySelector(".guess").value);
+document.querySelector(".guess").value = 23; */
+
+// Implementing the Game Logic
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+// Refactoring Our Code: The DRY Principle
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+const displayNumber = function (number) {
+  document.querySelector(".number").textContent = number;
+};
+const displayScore = function (score) {
+  document.querySelector(".score").textContent = score;
+};
+
+// Handling Click Event
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+
+  if (!guess) {
+    displayMessage("⛔️ No Number!");
+    // Manipulating CSS Styles
+  } else if (guess === secretNumber) {
+    displayMessage("🎉 Correct Number!");
+    displayNumber(secretNumber);
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  }
+  // Refactoring
+  else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
+      score--;
+      displayScore(score);
+    } else {
+      displayMessage("💥 You lost the game!");
+      displayScore(0);
+    }
+  }
+  /* else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📉 Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+  } */
+});
+
+// Coding Challenge #1
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage("Start guessing...");
+  displayScore(score);
+  displayNumber("?");
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});

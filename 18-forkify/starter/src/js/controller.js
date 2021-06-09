cdeaa2c260bb71fc16baf84a0230c9ddd42855d0
Sol-Ahn <sorry0194@gmail.com>
2021-06-10 01:31:08 +0900

    Add: section 18 (in progress)

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -1,13 +1,45 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from "./model.js";
+import recipeView from "./views/recipeView.js";
+
+import icons from "../img/icons.svg"; // Parcel 1
+// import icons from "url:../img/icons.svg"; // Parcel 2
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+const recipeContainer = document.querySelector(".recipe");
 
 const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+	return new Promise(function (_, reject) {
+		setTimeout(function () {
+			reject(new Error(`Request took too long! Timeout after ${s} second`));
+		}, s * 1000);
+	});
 };
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function () {
+	try {
+		const id = window.location.hash.slice(1);
+
+		if (!id) return;
+
+		recipeView.renderSpinner();
+
+		// 1) Loading recipe
+		await model.loadRecipe(id);
+
+		// 2) Rendering recipe
+		recipeView.render(model.state.recipe);
+	} catch (err) {
+		alert(err);
+	}
+};
+
+// window.addEventListener("hashchange", controlRecipes);
+// window.addEventListener("load", controlRecipes);
+["hashchange", "load"].forEach((ev) =>
+	window.addEventListener(ev, controlRecipes)
+);
